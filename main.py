6141964f161e2cc68982b33195b75745e2ca2247
Arohisharma007 <arohiwork07@gmail.com>
2025-07-14 08:17:12 +0530

    shopping useless files deleted

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ h2 = tk.Label(window, text='|< P.C.M >|', fg='pink', width=10, bg="teal",font=("
 h2.place(x=180,y=15)
 
 #PCM IMAGE
-image = Image.open("C:\\Users\\ASUS\\OneDrive\\Desktop\\PROJECT\\HAND_GESTURE\\.vscode\\hand.png")
+image = Image.open("C:\\Users\\ASUS\\OneDrive\\Desktop\\PROJECT\\HAND_GESTURE\\hand.png")
 # Resize the image using resize() method
 resize_image = image.resize((600, 900))
 img = ImageTk.PhotoImage(resize_image)
@@ -27,7 +27,7 @@ label1.place(x=453,y=380)
 #MEDIA PALYER
 b1=tk.Button(window,text="MIDEA PLAYER" ,font=("Bahnschrift SemiBold SemiConden",25),bg="light yellow",fg="brown",command=MediaPlayer.media)
 b1.place(x=10,y=600)
-image = Image.open("C:\\Users\\ASUS\\OneDrive\\Desktop\\PROJECT\\HAND_GESTURE\\.vscode\\MEDIA.png")
+image = Image.open("C:\\Users\\ASUS\\OneDrive\\Desktop\\PROJECT\\HAND_GESTURE\\MEDIA.png")
 # Resize the image using resize() method
 resize_image = image.resize((350, 350))
 img = ImageTk.PhotoImage(resize_image)
@@ -38,7 +38,7 @@ label1.place(x=50,y=240)
 #MOUSE
 b2=tk.Button(window,text="  MOUSE  " ,font=("Bahnschrift SemiBold SemiConden",25),bg="light yellow",fg="brown" , width=13,command=Mouse.mouse)
 b2.place(x=1070,y=600)
-image = Image.open("C:\\Users\\ASUS\\OneDrive\\Desktop\\PROJECT\\HAND_GESTURE\\.vscode\\MOUSE.png")
+image = Image.open("C:\\Users\\ASUS\\OneDrive\\Desktop\\PROJECT\\HAND_GESTURE\\mouse.png")
 # Resize the image using resize() method
 resize_image = image.resize((350, 350))
 img = ImageTk.PhotoImage(resize_image)
@@ -50,7 +50,7 @@ label1.place(x=1110,y=240)
 #SHOPPING SITE
 b3=tk.Button(window,text="SHOPPING SITE" ,font=("Bahnschrift SemiBold SemiConden",25),bg="light yellow",fg="brown",command=Shopping.shopping)
 b3.place(x=10,y=1350)
-image = Image.open("C:\\Users\\ASUS\\OneDrive\\Desktop\\PROJECT\\HAND_GESTURE\\.vscode\\shopping.png")
+image = Image.open("C:\\Users\\ASUS\\OneDrive\\Desktop\\PROJECT\\HAND_GESTURE\\shopping.png")
 # Resize the image using resize() method
 resize_image = image.resize((350, 350)) 
 img = ImageTk.PhotoImage(resize_image)
@@ -62,7 +62,7 @@ label1.place(x=50,y=990)
 #OTHERS
 b4=tk.Button(window,text="OTHER" ,font=("Bahnschrift SemiBold SemiConden",25),bg="light yellow",fg="brown" ,width=13,command=other.other)
 b4.place(x=1070,y=1350)
-image = Image.open("C:\\Users\\ASUS\\OneDrive\\Desktop\\PROJECT\\HAND_GESTURE\\.vscode\\other.png")
+image = Image.open("C:\\Users\\ASUS\\OneDrive\\Desktop\\PROJECT\\HAND_GESTURE\\other.png")
  # Resize the image using resize() method
 resize_image = image.resize((350, 350)) 
 img = ImageTk.PhotoImage(resize_image)
@@ -75,7 +75,3 @@ label1.place(x=1110,y=990)
 window.geometry("1500x1500")
 window.configure(bg="teal")
 window.mainloop()
-
-
-#SHOPPING SITE LINK
-#https://www.flipkart.com/
